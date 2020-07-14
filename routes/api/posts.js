0630267a31a1d8f4e509ed8565c8e332191d096c
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-14 16:39:41 +0530

    Delete post done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const Post = require("../../models/Post");
 const validatePostInput = require("../../validation/post");
+const Profile = require("../../models/Profile");
 // router.get("/test", (req, res) => {
 //   res.send("Posts Works here");
 // });
@@ -30,6 +31,54 @@ router.post(
     });
 
     newPost.save().then((post) => res.json(post));
+  }
+);
+
+// GET api/posts
+// get all posts
+// Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(404).json({ nopostsfound: "No posts found" }));
+});
+
+// GET api/posts/:id
+// get post by id
+// Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => res.json(post))
+    .catch((err) =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
+
+// DELETE api/posts/:id
+// Delete post
+// Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          //check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          //delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch((err) =>
+          res.status(404).json({ postnotfound: "No post found" })
+        );
+    });
   }
 );
 
