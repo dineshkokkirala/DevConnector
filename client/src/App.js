e103a8a2e1a76cfb78a6ef4bba006e140b9c78d2
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-15 18:48:35 +0530

    Register and login schema ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
