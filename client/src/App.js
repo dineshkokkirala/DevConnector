9fb21b524116443eda314edb880967782e340239
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-17 17:12:37 +0530

    Add Experience form created

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import { clearCurrentProfile } from "./actions/profileActions";
 import PrivateRoute from "./components/common/PrivateRoute";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
 
 //check for token
 if (localStorage.jwtToken) {
@@ -65,6 +66,13 @@ function App() {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+            </Switch>
+            <Switch>
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
               />
             </Switch>
           </div>
