debf0fe4b53e66a7a6f744788ff0fde5f8c0f73d
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-18 15:19:04 +0530

    Profile about done

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -32,7 +32,7 @@ class Profile extends Component {
             <div className="col-md-6"></div>
           </div>
           <ProfileHeader profile={profile} />
-          <ProfileAbout />
+          <ProfileAbout profile={profile} />
           <ProfileCreds />
           <ProfileGithub />
         </div>
