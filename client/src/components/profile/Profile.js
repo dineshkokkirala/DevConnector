a4f2037924f17d090b835ce15d7eee082b881f4e
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-18 15:38:29 +0530

    Profile credentials done

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -33,7 +33,10 @@ class Profile extends Component {
           </div>
           <ProfileHeader profile={profile} />
           <ProfileAbout profile={profile} />
-          <ProfileCreds />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
           <ProfileGithub />
         </div>
       );
