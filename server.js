d2dc35afc371a06161c7d7a49cddaca0bd89ffd3
Dinesh Venkata Sai <dineshkokkirala369@gmail.com>
2020-07-19 12:22:57 +0530

    Ready to deploy

--- server.js
+++ server.js
@@ -42,8 +42,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// app.get("/", (req, res) => res.send("Hello dev Connectors!!!"));
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
